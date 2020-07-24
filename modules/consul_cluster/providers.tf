3ae7d61e3da91b86f0c5f6c768625a4e4fbaedea
Alvin Huang <17609145+alvin-huang@users.noreply.github.com>
2020-07-24 15:45:16 -0400

    pin provider versions in terraform block instead

--- modules/consul_cluster/providers.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  version = "~> 2.70.0"
-}
-
-provider "random" {
-  version = "~> 2.3.0"
-}
--- modules/consul_cluster/versions.tf
+++ modules/consul_cluster/versions.tf
@@ -1,4 +1,8 @@
 # names required TF version
 terraform {
   required_version = ">= 0.12"
+  required_providers {
+    aws    = "~> 2.70.0"
+    random = "~> 2.3.0"
+  }
 }
